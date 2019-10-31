3aa6780025f543bd30db75dc9db597af772c3cb7
Lamia <lamiakais1449@gmail.com>
2019-10-31 18:39:04 +0100

    hello

--- Vue.java
+++ Vue.java
@@ -1,5 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
-public class Vue extends JPanel implements ActionListener{
+import java.awt.event.*;
+public class Vue extends JFrame implements ActionListener{
+protected JPanel pan;
+protected static JButton jouer,recommencer,quitter;
+
+public Vue(){
+this.setTitle("Azul");
+this.pan = new JPanel();
+this.jouer = new JButton("Jouer");
+this.recommencer = new JButton("Recommencer");
+this.quitter = new JButton("Quitter");
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+this.setSize(800,800);
+this.setResizable(true);
+this.setLocationRelativeTo(null);
+this.pan.add(jouer);
+this.pan.add(recommencer);
+this.pan.add(quitter);
+this.setContentPane(pan);
+this.setVisible(true);
+}
+
+
+public static void main(String[] args){
+Vue vue = new Vue();
+}
+
+public void actionPerformed(ActionEvent clic){
+
+}
 
 } 
