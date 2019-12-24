81f3148042afb7594e6bc6ea34a14167bec3db6e
Lamia <lamiakais1449@gmail.com>
2019-12-24 19:43:59 +0100

    sans accent

--- Jeu.java
+++ Jeu.java
@@ -4,13 +4,8 @@ import java.util.*;
 
 public class Jeu {
 
-	protected static Vue vue;
-
 	protected static JeuVue jeu;
-
 	protected static JFrame fenetre;
-
-	
 
 	
 
@@ -19,36 +14,29 @@ public class Jeu {
 	System.out.println("Souhaitez-vous jouer en textuel ou en interface graphique ?\n1 - textuel \n 2 - interface graphique");
 
 		Scanner sc = new Scanner(System.in);
-
 		int tmp = sc.nextInt();
 
 		if(tmp == 1){
 
 		Manche m = new Manche();
-
 		m.commence();
 
 
 		}else if(tmp == 2){
 
 		fenetre = new JFrame("Azul");
-
 		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		fenetre.setSize(800,800);
-
 		fenetre.setResizable(true);
-
 		fenetre.setLocationRelativeTo(null);
+		
+		jeu.joueursInfo();
 
 		}else{
 
 		System.out.println("Veuillez saisir une donnee coherente");
-
 		choix();
-
 		}
-
 	}
 
 	
@@ -56,15 +44,6 @@ public class Jeu {
 	
 
 	public static void main(String[] args) {
-
-		choix();
-
-		
-
+		choix();		
 	}
-
-	
-
-	
-
 }
