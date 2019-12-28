622677f3bd558b204681842c8fce17ceeb3f7a51
Lamia <lamiakais1449@gmail.com>
2019-12-28 21:44:02 +0100

    update

--- MurVue.java
+++ MurVue.java
@@ -9,4 +9,25 @@ public class MurVue{
 	this.tuilesposees4 = new LinkedList<TuilesVue>();
 	this.tuilesposees5 = new LinkedList<TuilesVue>();
     }
+
+
+    public void murTuile1(TuilesVue tuile){
+	if(tuilesposees1.size() == 0){
+	    tuilesposees1.add(tuile);
+	    System.out.println("Tuile ajoute a la ligne 1 du mur");
+	}else if(tuilesposees1.size()>0){
+	    boolean b = true; 
+	    for(int i = 0;i<tuilesposees1.size();i++){
+		if(tuile.couleur.equals(tuilesposees1.get(i).couleur)){
+		    b = false;
+		}
+	    }
+	    if(b){
+		tuilesposees1.add(tuile);
+	    }
+	}else{
+	    System.out.println("La ligne 1 du mur est pleine !");
+	}
+    }
+    
 }
