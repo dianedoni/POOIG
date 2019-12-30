a6e09e35a95a330285428a79a89644bc92297883
Lamia <lamiakais1449@gmail.com>
2019-12-30 22:49:54 +0100

    apap

--- TuilesVue.java
+++ TuilesVue.java
@@ -4,26 +4,38 @@ public class TuilesVue{
     protected String couleur;
     protected int posX,posY;
     protected Image imgt;
+    protected JButton bouton;
     protected ImageIcon icot;
     protected int nb_max_tuiles = 100;
 
+
+    //mettre les posX et posY
     public TuilesVue(String c){
 	if(nb_max_tuiles >0){	
 	    nb_max_tuiles -=1;
 	    if(c.equals("blanc")){
 		this.icot = new ImageIcon(getClass().getResource("Image/Blanc.png"));
+		this.bouton = new JButton(icot);
 		this.imgt = this.icot.getImage();
 	    }else if(c.equals("bleu")){
 		this.icot = new ImageIcon(getClass().getResource("Image/Bleu.png"));
+		this.bouton = new JButton(icot);
 		this.imgt = this.icot.getImage();
 	    }else if(c.equals("jaune")){
 		this.icot = new ImageIcon(getClass().getResource("Image/Jaune.png"));
+		this.bouton = new JButton(icot);
 		this.imgt = this.icot.getImage();
 	    }else if(c.equals("noir")){
 		this.icot = new ImageIcon(getClass().getResource("Image/Noir.png"));
+		this.bouton = new JButton(icot);
 		this.imgt = this.icot.getImage();
 	    }else if(c.equals("rouge")){
 		this.icot = new ImageIcon(getClass().getResource("Image/Rouge.png"));
+		this.bouton = new JButton(icot);
+		this.imgt = this.icot.getImage();
+	    }else if(c.equals("vide")){
+		this.icot = new ImageIcon(getClass().getResource("Image/Vide.png"));
+		this.bouton = new JButton(icot);
 		this.imgt = this.icot.getImage();
 	    }
 	}
